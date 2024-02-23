e52079f8c4ec3cfc7bc41412c5d6f580c5a6d57d
Dev Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2024-02-23 15:52:55 +0530

    Update uni_cost_src.py
    with path

--- Lab5/uni_cost_src.py
+++ Lab5/uni_cost_src.py
@@ -1,40 +1,52 @@
-class graph:
-    def __init__(self,n):
-        self.list={}
-        self.mat=[[0 for x in range (n)] for _ in range (n)]
-        self.size=n
-
-    def create_mat (self):
-        for node in self.list:
-            for des,wt in self.list[node]:
-                self.mat[node][des]=wt
-
-    def add_edge(self, src , des):
-        self.list[src]=des
-
-    def display(self):
-        print(self.mat)
-        print(self.list)
 
 
+def ucs( graph, goal , source  ):
+    pri_queue = []
+    previous = {}
+    visited = set ()
 
-def uni_cost(g):
-    pri_queue=[(0,0)]
-    # nodes=g.list[0]
-    visited=[]
-    curr_cost = 0
+    pri_queue.append((source,"", 0))
+    while pri_queue:
 
-    while (len(visited)<= g.size):
-        print(pri_queue)
-        nodes=pri_queue.pop(0)
-        visited.append(nodes[0])
-        # curr_cost+=nodes[1]
-        print(nodes)
+        pri_queue.sort ( key = lambda x:x[2])
+        current,prev,cost = pri_queue.pop(0)
 
-        for node,_ in g.list[nodes[0]]:
-            if node in visited:
-                continue
-            pri_queue.append((node , _ +nodes[1]))
-        pri_queue.sort(key= lambda x:x[1])
+        if current  in visited:
+            continue
 
-    return nodes[1]
+        visited.add(current)
+        previous[current]=prev
+
+        if current == goal:
+            print(cost)
+            return path (previous,source , goal)
+        for des,ct in graph[current]:
+            if des not in visited:
+                pri_queue.append((des , current , ct+cost  ))
+
+
+
+
+
+
+
+
+def path ( previous ,src , goal ):
+    path = []
+    current = goal
+    path.append(current)
+    while current != src :
+        current = previous[current]
+        path.append(current)
+    return path[::-1]
+
+graph = {
+  'S': [('1', 2), ('3', 5)],
+  '1': [('G', 1)],
+  '2': [('1', 4)],
+  '3': [('1', 5), ('G', 6), ('4', 2)],
+  '4': [('5', 3), ('2', 4)],
+  '5': [('G', 3), ('2', 6)],
+  'G': [('4', 7)]
+}
+print(ucs(graph,'G','S'))
