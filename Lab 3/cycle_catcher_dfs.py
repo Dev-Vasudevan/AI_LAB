69e2995f872c0263707478b1e2454ecb559e22aa
Dev Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2024-02-29 21:27:22 +0530

    Create cycle_catcher_dfs.py

---
+++ Lab 3/cycle_catcher_dfs.py
@@ -0,0 +1,57 @@
+class graph :
+    def __init__(self , n ):
+        graph.size = n
+        graph.mat =[[0*n] for _ in range (n)]
+        graph.list = {}
+def cycle_starter ( graph ):
+    visited = set()
+    for i in range (graph.size):
+        if i not in visited :
+            if (cycle_catcher(i,graph,visited,[])):
+                return True
+
+    return False
+
+
+
+
+def cycle_catcher ( node , graph , visited , path):
+    if node  in visited:
+        return
+
+    visited.add(node)
+
+    flag=True
+    for des in graph.list[node]:
+
+        if des in path :
+            print(path + [node])
+            return True
+        if des not in visited:
+            flag = False
+            path.append(node)
+            if (cycle_catcher ( des , graph , visited , path)):
+                return True
+    if path and flag :
+        path.pop()
+
+    return False
+
+if __name__== "__main__" :
+    g= graph(6)
+    g.list={
+        0:[1] ,
+        1:[],
+        2:[1,3],
+        3:[4],
+        4:[5],
+        5:[3]
+
+    }
+    print(cycle_starter(g))
+
+
+
+
+
+
